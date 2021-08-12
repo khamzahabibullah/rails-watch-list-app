9b765f36d4cea8e2bd86bb7ed14bc48160966060
Khamza Habibullah <habibullahkhamza@gmail.com>
2021-08-12 12:18:11 +0400

    added dependency destroy to review

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,7 @@ class List < ApplicationRecord
   has_one_attached :photo
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true
   validates :name, presence: true
 end
